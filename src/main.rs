7de862a1592c76a1b4eac3184f6d74310a9c90ac
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2026-08-12 14:43:08 +0200

    Updates.

--- src/main.rs
+++ src/main.rs
@@ -24,26 +24,22 @@ fn bench_table_grow(initial: usize, grow: usize, iterations: usize) {
   let store = wasmer::Store::new(wasmer::sys::Singlepass::default());
   let module = wasmer::Module::from_binary(&store, &wasm_bytes).unwrap();
   let mut ticks = vec![];
-  let mut times = vec![];
   for _ in 1..=iterations {
     let compiler = wasmer::sys::Singlepass::default();
     let mut store = wasmer::Store::new(compiler);
     let instance = wasmer::Instance::new(&mut store, &module, &wasmer::imports! {}).unwrap();
     let fun = instance.exports.get_typed_function::<(), i32>(&store, "fun").unwrap();
-    let t_start = std::time::Instant::now();
     let start = clock.raw();
     _ = fun.call(&mut store);
     let end = clock.raw();
-    let t_end = std::time::Instant::now();
     ticks.push(clock.delta(start, end).as_nanos() as u64);
-    times.push((t_end - t_start).as_nanos() as u64);
   }
   let median_ticks = medianu64(&mut ticks).unwrap();
-  let median_times = medianu64(&mut times).unwrap();
-  println!("initial = {}, grow = {}, ticks {}, times {}", initial, grow, median_ticks, median_times);
+  println!("initial = {}, grow = {}, {}", initial, grow, median_ticks);
 }
 
 fn main() {
+  core_affinity::set_for_current(core_affinity::get_core_ids().unwrap()[0]);
   let args = std::env::args().skip(1).collect::<Vec<String>>();
   if args.len() != 3 {
     eprintln!("invalid number of arguments");
